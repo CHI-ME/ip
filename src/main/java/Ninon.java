68974fb31904cf69dbfb3fbd0ce3ab12a7161d3b
ChenFan <e1129819@u.nus.edu>
2025-01-24 14:38:08 +0800

    rename, add greeting and exit

---
+++ src/main/java/Ninon.java
@@ -0,0 +1,10 @@
+public class Ninon {
+
+    public static final String SPLIT = "____________________________________________________________\n";
+    public static void main(String[] args) {
+        String greet = "Hello! I'm NINON\n" +
+                "What can I do for you?\n";
+        String exit = "Bye. Hope to see you again soon!\n";
+        System.out.println(SPLIT + greet + SPLIT + exit + SPLIT);
+    }
+}
