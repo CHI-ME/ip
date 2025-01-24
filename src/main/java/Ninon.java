131d5716d22a3e0a08afdca639094215d57ada34
ChenFan <e1129819@u.nus.edu>
2025-01-24 14:53:39 +0800

    add echo function

--- src/main/java/Ninon.java
+++ src/main/java/Ninon.java
@@ -1,10 +1,27 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Ninon {
 
     public static final String SPLIT = "____________________________________________________________\n";
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         String greet = "Hello! I'm NINON\n" +
                 "What can I do for you?\n";
         String exit = "Bye. Hope to see you again soon!\n";
-        System.out.println(SPLIT + greet + SPLIT + exit + SPLIT);
+
+        System.out.println(SPLIT + greet + SPLIT);
+        String message = input.nextLine();
+
+        while (!Objects.equals(message, "bye")) {
+            String respond = echo(message);
+            System.out.println(SPLIT + respond + "\n" + SPLIT);
+            message = input.nextLine();
+        }
+
+        System.out.println(SPLIT + exit + SPLIT);
+    }
+
+    public static String echo(String message) {
+        return message;
     }
 }
