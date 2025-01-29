9e0e83a45bc9b950563fa205f22117c74e8ac7fc
ChenFan <e1129819@u.nus.edu>
2025-01-30 02:37:38 +0800

    add add_List() and print_List() function

--- src/main/java/Ninon.java
+++ src/main/java/Ninon.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
+
 public class Ninon {
 
     public static final String SPLIT = "____________________________________________________________\n";
+    public static ArrayList<String> list = new ArrayList<String>(100);
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String greet = "Hello! I'm NINON\n" +
@@ -12,8 +15,15 @@ public class Ninon {
         System.out.println(SPLIT + greet + SPLIT);
         String message = input.nextLine();
 
-        while (!Objects.equals(message, "bye")) {
-            String respond = echo(message);
+        while (true) {
+            String respond;
+            if (Objects.equals(message, "bye")) {
+                break;
+            } else if (Objects.equals(message, "list")) {
+                respond = print_List();
+            } else {
+                respond = add_List(message);
+            }
             System.out.println(SPLIT + respond + "\n" + SPLIT);
             message = input.nextLine();
         }
@@ -22,6 +32,20 @@ public class Ninon {
     }
 
     public static String echo(String message) {
+        return message;
+    }
+
+    public static String add_List(String message) {
+        list.add(message);
+        return "added " + message;
+    }
+
+    public static String print_List() {
+        String message = "";
+        for (int i = 0; i < list.size(); i++) {
+            message += (i + 1);
+            message += ". " + list.get(i) + "\n";
+        }
         return message;
     }
 }
