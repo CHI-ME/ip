7e15b3ed5337fc82a1eec05a18c7c40b3fa9377e
ChenFan <e1129819@u.nus.edu>
2025-01-30 17:17:35 +0800

    add a common used exception class DukeException

--- src/main/java/Ninon.java
+++ src/main/java/Ninon.java
@@ -17,32 +17,54 @@ public class Ninon {
 
         while (true) {
             String respond;
-            if (Objects.equals(message, "bye")) {
-                break;
-            } else if (Objects.equals(message, "list")) {
-                respond = list.toString();
-            } else if (Objects.equals(message.split(" ")[0], "mark")) {
-                respond = "Nice! I've marked this task as done:\n";
-                respond += list.mark(Integer.parseInt(message.split(" ")[1]));
-            } else if (Objects.equals(message.split(" ")[0], "unmark")) {
-                respond = "Nice! I've marked this task as not done yet:\n";
-                respond += list.unmark(Integer.parseInt(message.split(" ")[1]));
-            } else if (Objects.equals(message.split(" ")[0], "todo")) {
-                String description = message.split("/")[0].replace("todo ","");
-                respond = list.add_List(new Todo(description));
-            } else if (Objects.equals(message.split(" ")[0], "deadline")) {
-                String description = message.split("/")[0].replace("deadline ","");
-                String by = message.split("/")[1].replace("by ","");
-                respond = list.add_List(new Deadline(description, by));
-            } else if (Objects.equals(message.split(" ")[0], "event")) {
-                String description = message.split("/")[0].replace("event ","");
-                String from = message.split("/")[1].replace("from ","");
-                String to = message.split("/")[2].replace("to ","");
-                respond = list.add_List(new Event(description, from, to));
-            } else {
-                respond = list.add_List(new Task(message));
+            try {
+                if (Objects.equals(message, "bye")) {
+                    break;
+                } else if (Objects.equals(message, "list")) {
+                    respond = list.toString();
+                } else if (Objects.equals(message.split(" ")[0], "mark")) {
+                    respond = "Nice! I've marked this task as done:\n";
+                    respond += list.mark(Integer.parseInt(message.split(" ")[1]));
+                } else if (Objects.equals(message.split(" ")[0], "unmark")) {
+                    respond = "Nice! I've marked this task as not done yet:\n";
+                    respond += list.unmark(Integer.parseInt(message.split(" ")[1]));
+                } else if (Objects.equals(message.split(" ")[0], "todo")) {
+                    if (message.split(" ").length == 1) {
+                        throw new DukeException("OOPS!!! The description of a todo cannot be empty.");
+                    } else {
+                        String description = message.split("/")[0].replace("todo ","");
+                        respond = list.add_List(new Todo(description));
+                    }
+                } else if (Objects.equals(message.split(" ")[0], "deadline")) {
+                    if (message.split(" ").length == 1) {
+                        throw new DukeException("OOPS!!! The description of a deadline cannot be empty.");
+                    } else if (message.split("/").length == 1) {
+                        throw new DukeException("OOPS!!! The by of a deadline cannot be empty.");
+                    } else {
+                        String description = message.split("/")[0].replace("deadline ","");
+                        String by = message.split("/")[1].replace("by ","");
+                        respond = list.add_List(new Deadline(description, by));
+                    }
+                } else if (Objects.equals(message.split(" ")[0], "event")) {
+                    if (message.split(" ").length == 1) {
+                        throw new DukeException("OOPS!!! The description of a event cannot be empty.");
+                    } else if (message.split("/").length == 1) {
+                        throw new DukeException("OOPS!!! The from of a event cannot be empty.");
+                    } else if (message.split("/").length == 2) {
+                        throw new DukeException("OOPS!!! The to of a event cannot be empty.");
+                    } else {
+                        String description = message.split("/")[0].replace("event ","");
+                        String from = message.split("/")[1].replace("from ","");
+                        String to = message.split("/")[2].replace("to ","");
+                        respond = list.add_List(new Event(description, from, to));
+                    }
+                } else {
+                    throw new DukeException("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+                System.out.println(SPLIT + respond + "\n" + SPLIT);
+            } catch (DukeException exception) {
+                System.out.println(SPLIT + exception.getMessage() + "\n" + SPLIT);
             }
-            System.out.println(SPLIT + respond + "\n" + SPLIT);
             message = input.nextLine();
         }
 
